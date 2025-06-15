52ebcf54726945cee3453e42c7a06ea5609b2ef2
Ram Perey <ramonperey2490@gmail.com>
2025-06-15 14:06:17 +0800

    Update js for register

--- js/register.js
+++ js/register.js
@@ -21,6 +21,51 @@ registerForm.onsubmit = (e) => {
         body: JSON.stringify(data)
     }
 
+                document
+            .getElementById("register-button")
+            .addEventListener("click", function (event) {
+                event.preventDefault();
+
+                const username = document.querySelector(".registerName").value.trim();
+                const password = document.querySelector(".registerPass").value.trim();
+                const confirmPassword = document
+                    .querySelector(".registerConfirm")
+                    .value.trim();
+                const errorElement = document.getElementById("passwordError");
+
+                errorElement.textContent = "";
+                const passwordPattern = /^[A-Z]/;
+
+                if (username === "" || password === "" || confirmPassword === "") {
+                    alert("Mag fill up ka muna.");
+                    return;
+                }
+
+                if (password !== confirmPassword) {
+                    alert("passwords do not match.");
+                    return;
+                }
+
+                if (password.length < 8) {
+                    errorElement.classList.toggle("active");
+                    errorElement.textContent = "Password must be 8 characters.";
+                    return;
+                }
+
+                if (!passwordPattern.test(password)) {
+                    errorElement.classList.toggle("active");
+                    errorElement.textContent = "Use uppercase for the first character.";
+                    return;
+                }
+
+                localStorage.setItem("registeredUsername", username);
+                localStorage.setItem("registeredPassword", password);
+
+                alert("Registration successful!");
+                toggleForms();
+            });
+
+
     fetch('/register', options)
         .then(res => res.json())
         .then(data => {
